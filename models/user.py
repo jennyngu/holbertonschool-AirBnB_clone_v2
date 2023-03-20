b38fd384d85095bf1be4d408e195dafa032e98b2
Jenny <5704@holbertonstudents.com>
2023-03-20 16:42:47 -0700

    Added an attribute that reps a relationship with Place

--- models/user.py
+++ models/user.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -14,3 +15,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+
+    places = relationship("Place", backref="user", cascade="all, delete")
