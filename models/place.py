03746ba5244373d54b655d3d70140f4501091309
bigBadMatt <5707@holbertonstudents.com>
2023-03-25 21:39:41 -0700

    added getter method to build amenity and review list for filestorage

--- models/place.py
+++ models/place.py
@@ -18,3 +18,27 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
+
+if getenv("HBNB_TYPE_STORAGE", None) != "db":
+    @property
+    def reviews(self):
+        """get a list of linked reviews"""
+        review_list = []
+        for review in list(models.storage.all(Review).values()):
+            if review.place_id == self.id:
+                review_list.append(review)
+        return review_list
+
+    @property
+    def amenities(selfl):
+        """get amenities"""
+        amenity_list = []
+        for amenity in list(models.storage.all(Amenity).values()):
+            if amenity.id in self.amenity_ids:
+                amenity_list.append(amenity)
+        return amenity_list
+
+    @amenities.setter
+    def amenities(self, value):
+        if type(value) == Amenity:
+            self.amenity_ids.append(value.id)
