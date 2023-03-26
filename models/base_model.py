dd2482acfd6a5e8c6640180d4a32b882a760639b
bigBadMatt <5707@holbertonstudents.com>
2023-03-26 04:03:16 -0700

    changed order of parsing kwargs to try prevent skipping some

--- models/base_model.py
+++ models/base_model.py
@@ -25,20 +25,18 @@ class BaseModel:
         """
 
         self.id=str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        self.created_at = self.updated_at = datetime.utcnow()
 
         if kwargs:
             for key, value in kwargs.items():
-                if key != '__class__':
-                    setattr(self, key, value)
                 if value == "created_at":
                     self.created_at = datetime.strptime(self.created_at,
                                                  '%Y-%m-%dT%H:%M:%S.%f')
                 if value == "updated_at":
                     self.updated_at = datetime.strptime(self.updated_at,
                                                  '%Y-%m-%dT%H:%M:%S.%f')
-
+                if key != '__class__':
+                    setattr(self, key, value)
 
     def __str__(self):
         """
