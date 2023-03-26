67744c05b68fa702f3e0f37693271cf658df7f32
bigBadMatt <5707@holbertonstudents.com>
2023-03-26 04:26:07 -0700

    reverted changes

--- models/place.py
+++ models/place.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from os import getenv
-import models
 from models.base_model import BaseModel, Base
 from models.amenity import Amenity
 from models.review import Review
