044c75c5e4dc66bf84e0a1672595829231711846
bigBadMatt <5707@holbertonstudents.com>
2023-03-25 21:56:47 -0700

    added missing import

--- models/place.py
+++ models/place.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
+from os import getenv
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
