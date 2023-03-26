877602db0c38569f5c5815f8e52f55dc86763e5f
bigBadMatt <5707@holbertonstudents.com>
2023-03-26 04:37:35 -0700

    fixed formatting to satisfy linter

--- models/base_model.py
+++ models/base_model.py
@@ -23,21 +23,20 @@ class BaseModel:
         """
         Instantiates a new model
         """
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.utcnow()
-            storage.new(self)
-        else:
-            for k, v in kwargs.items():
-                if k != '__class__':
-                    setattr(self, k, v)
-            self.created_at = datetime.strptime(self.created_at,
-                                                '%Y-%m-%dT%H:%M:%S.%f')
-            self.updated_at = datetime.strptime(self.updated_at,
-                                                '%Y-%m-%dT%H:%M:%S.%f')
-            if not hasattr(self, 'id'):
-                self.id = str(uuid.uuid4())
+
+        self.id=str(uuid.uuid4())
+        self.created_at = self.updated_at = datetime.utcnow()
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if value == "created_at":
+                    self.created_at = datetime.strptime(self.created_at,
+                                                 '%Y-%m-%dT%H:%M:%S.%f')
+                if value == "updated_at":
+                    self.updated_at = datetime.strptime(self.updated_at,
+                                                 '%Y-%m-%dT%H:%M:%S.%f')
+                if key != '__class__':
+                    setattr(self, key, value)
 
     def __str__(self):
         """
